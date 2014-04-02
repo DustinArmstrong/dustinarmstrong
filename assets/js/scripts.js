76bc636256bfb102e1aa44852f612a052507264e
Dustin Armstrong <dustinarmstrong@me.com>
2014-04-02 03:24:25 -0400

    Started over since I left the code at work and didn't want to wait.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,12 @@
 $(document).ready(
-	$('button.flat').click(function() {
-		$('body').removeClass().addClass("flat");
-	}),
-	$('button.simple').click(function() {
-		$('body').removeClass().addClass("simple");
+	$('button.hide-introduction').click(function() {
+		//$('.hide-introduction').removeClass().addClass("flat");
+		$('.introduction').addClass('animated fadeOutUpBig');
+		$('.introduction').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+			$('.introduction').addClass('hidden');
+		});
+		$('.avatar').addClass('animated flipInX');
+		$('header h1').addClass('animated slideInDown');
+		$('.content').addClass('animated slideInLeft');
 	})
 );
---
+++ index.markdown
@@ -0,0 +1,8 @@
+---
+layout: default
+title: Front-End Designer &amp; Developer
+---
+
+I make websites &amp; web applications that focus on usability, coherence, and flexibility. 
+
+Every project deserves to be built responsively and in code that is not only valid, but clean and expandable.
