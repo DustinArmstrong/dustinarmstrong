4d734d2dfe34781e911982fd8a18c812b1d900a1
Dustin Armstrong <dustinarmstrong@me.com>
2014-04-09 20:36:32 -0400

    Removed un-needed js, updated post format, fixed blog index page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,9 @@
 $(document).ready(
-	// $('button.hide-introduction').click(function() {
-		//$('.hide-introduction').removeClass().addClass("flat");
-		// $('.introduction').addClass('animated fadeOutUpBig'),
-		// $('.introduction').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-		// 	$('.introduction').addClass('hidden');
-		// }),
-		$('body.blue nav').addClass('animated slideInRight'),
-		$('body.blue .avatar').addClass('animated flipInX'),
-		$('body.blue .main-header h1, body.blue .main-header .h1').addClass('animated slideInDown'),
-		$('body.blue .section:nth-child(odd)').addClass('animated slideInLeft'),
-		$('body.blue .section:nth-child(even)').addClass('animated slideInRight'),
-	// }),
+	$('body.blue nav').addClass('animated slideInRight'),
+	$('body.blue .avatar').addClass('animated flipInX'),
+	$('body.blue .main-header h1, body.blue .main-header .h1').addClass('animated slideInDown'),
+	$('body.blue .section:nth-child(odd)').addClass('animated slideInLeft'),
+	$('body.blue .section:nth-child(even)').addClass('animated slideInRight'),
 	$('nav.primary ul').click(function() {
 		if ($(this).hasClass('expand')) {
 			$(this).removeClass();
@@ -18,19 +11,4 @@ $(document).ready(
 			$(this).addClass('expand');
 		}
 	})
-	// $('a').scrollTo(),
-	// $('.contact-toggle').click(function() {
-	// 	if ($('#contact').hasClass('expand')) {
-	// 		$('#contact').removeClass('expand');
-	// 	} else {
-	// 		$('#contact').addClass('expand');
-	// 	}
-	// })	
-	// $('.avatar').click(function() {
-	// 	if ($('body').hasClass('blue')) {
-	// 		$('body').removeClass().addClass('pink');
-	// 	} else {
-	// 		$('body').removeClass().addClass('blue');
-	// 	}
-	// })
 );
