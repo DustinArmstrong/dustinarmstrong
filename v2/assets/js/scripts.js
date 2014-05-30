43695d1818c9ec67f8b41b8809a679db9217856d
Dustin Armstrong <dustin@dustinarmstrong.com>
2014-05-30 18:43:10 -0400

    Addition of V2 version of portfolio for professional submissions

---
+++ v2/assets/js/scripts.js
@@ -0,0 +1,25 @@
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+$(window).bind("load", function() {
+	$("body").removeClass("preload");
+	$('#topbar').addClass('animated slideInDown');
+	$('.avatar').addClass('animated slideInLeft');
+	$('.plus, .equals').addClass('animated rotateIn');
+	$('.saa-logo').addClass('animated slideInDown');
+	$('.heart').addClass('animated slideInRight');
+	$('.greeting').addClass('animated fadeIn');
+	$('.hire-me').addClass('animated slideInUp');
+});
